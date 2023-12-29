735c20572f902644b95f4be697a8a3045401a7ac
Jeff Patton <jeffrey@patton-tech.com>
2023-12-29 02:25:42 -0600

    Added NewPool Cmdlet closes #9

---
+++ Pipelines/Cmdlets/NewPool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Management.Automation;
+using ModPosh.Pipelines.Ado;
+
+namespace ModPosh.Pipelines.Cmdlets
+{
+    [Cmdlet(VerbsCommon.New, "Pool", HelpUri = "")]
+    [OutputType(typeof(Pool))]
+    [CmdletBinding(PositionalBinding = true)]
+    public class NewPool : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
+        public string Name { get; set; } = string.Empty;
+        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Default")]
+        public string[] Demands { get; set; } = Array.Empty<string>();
+
+        protected override void BeginProcessing()
+        {
+            Pool pool = new Pool(Name);
+            if (Demands.Length > 0)
+                pool.Demands = Demands;
+            WriteObject(pool);
+        }
+    }
+}
