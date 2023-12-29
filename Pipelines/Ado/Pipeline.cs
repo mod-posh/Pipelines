30ef220841e7d075d50d378ecde730481ec28bfb
Jeff Patton <jeffrey@patton-tech.com>
2023-12-29 02:31:23 -0600

    Added Pipeline Class and NewPipeline Cmdlet, closes #5

---
+++ Pipelines/Ado/Pipeline.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModPosh.Pipelines.Ado
+{
+    public class Pipeline
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<Stage> Stages { get; set; } = new List<Stage>();
+        public Pipeline () { }
+        public Pipeline (string name)
+        {
+            Name = name;
+        }
+        public Pipeline (string name, List<Stage> stages)
+        {
+            Name = name;
+            Stages = stages;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"name: {Name}");
+            sb.AppendLine("stages:");
+            foreach (Stage stage in Stages)
+            {
+                sb.AppendLine(stage.ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ Pipelines/Cmdlets/NewPipeline.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Management.Automation;
+using ModPosh.Pipelines.Ado;
+
+namespace ModPosh.Pipelines.Cmdlets
+{
+    [Cmdlet(VerbsCommon.New, "Pipeline", HelpUri = "")]
+    [OutputType(typeof(Pipeline))]
+    [CmdletBinding(PositionalBinding = true)]
+    public class NewPipeline : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
+        public string Name { get; set; } = string.Empty;
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
+        public Stage[] Stages { get; set; } = Array.Empty<Stage>();
+        protected override void BeginProcessing()
+        {
+            Pipeline pipeline = new Pipeline(Name);
+            if (Stages.Length > 0)
+                pipeline.Stages = Stages.ToList();
+            WriteObject(pipeline);
+        }
+    }
+}
