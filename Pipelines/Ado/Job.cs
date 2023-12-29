eb6fef668692e09a6141a8b6af695a1a8b50b608
Jeff Patton <jeffrey@patton-tech.com>
2023-12-29 01:27:35 -0600

    Updated Job with Pool, closes #2

--- Pipelines/Ado/Job.cs
+++ Pipelines/Ado/Job.cs
@@ -5,7 +5,7 @@ namespace ModPosh.Pipelines.Ado
     public class Job
     {
         public string Name { get; set; } = string.Empty;
-        public string Pool { get; set; } = string.Empty;
+        public Pool Pool { get; set; } = new Pool();
         public Dictionary<string,string> Variables { get; set; } = new Dictionary<string, string>();
         public List<Template> Steps { get; set; } = new List<Template>();
         public Job() { }
@@ -13,18 +13,18 @@ namespace ModPosh.Pipelines.Ado
         {
             Name = name;
         }
-        public Job(string name, string pool)
+        public Job(string name, Pool pool)
         {
             Name = name;
             Pool = pool;
         }
-        public Job(string name, string pool, Dictionary<string,string> variables)
+        public Job(string name, Pool pool, Dictionary<string,string> variables)
         {
             Name = name;
             Pool = pool;
             Variables = variables;
         }
-        public Job(string name, string pool, Dictionary<string,string> variables, List<Template> steps)
+        public Job(string name, Pool pool, Dictionary<string,string> variables, List<Template> steps)
         {
             Name = name;
             Pool = pool;
