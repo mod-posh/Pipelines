d14cde22ab1c3987fd7f770f3cce3b8a105a00c2
Jeff Patton <jeffrey@patton-tech.com>
2023-12-29 02:24:06 -0600

    Added NewTemplate Cmdlet closes #8

---
+++ Pipelines/Cmdlets/NewTemplate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Collections;
+using System.Management.Automation;
+using ModPosh.Pipelines.Ado;
+
+namespace ModPosh.Pipelines.Cmdlets
+{
+    [Cmdlet(VerbsCommon.New, "Template", HelpUri = "")]
+    [OutputType(typeof(Template))]
+    [CmdletBinding(PositionalBinding = true)]
+    public class NewTemplate : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
+        public string Name { get; set; } = string.Empty;
+        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Default")]
+        public Hashtable Parameters { get; set; } = new Hashtable();
+
+        protected override void BeginProcessing()
+        {
+            Template template = new Template(Name);
+            if (Parameters != null)
+            {
+                template.Parameters = Parameters
+                    .Cast<DictionaryEntry>()
+                    .Where(entry => entry.Key != null && entry.Value != null)
+                    .ToDictionary(entry => (string)entry.Key!, entry => (string)entry.Value!);
+            }
+            WriteObject(template);
+        }
+    }
+}
