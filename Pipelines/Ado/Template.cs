4255a171411191a6a00779fa242bc45810d39e99
Jeff Patton <jeffrey@patton-tech.com>
2023-12-29 01:24:12 -0600

    Added Template Class #3

---
+++ Pipelines/Ado/Template.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace ModPosh.Pipelines.Ado
+{
+    public class Template
+    {
+        public string Name { get; set; } = string.Empty;
+        public Dictionary<string,string> Parameters { get; set; } = new Dictionary<string, string>();
+        public Template() { }
+        public Template(string name)
+        {
+            Name = name;
+        }
+        public Template(string name, Dictionary<string,string> parameters)
+        {
+            Name = name;
+            Parameters = parameters;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"- template: {Name}");
+            if (Parameters.Count > 0)
+            {
+                sb.AppendLine($"  parameters:");
+                foreach (var parameter in Parameters)
+                {
+                    if (parameter.Value.StartsWith("$"))
+                        sb.AppendLine($"    {parameter.Key}: {parameter.Value}");
+                    else
+                        sb.AppendLine($"    {parameter.Key}: \"{parameter.Value}\"");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
