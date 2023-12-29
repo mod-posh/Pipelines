794681753aa489f555e769413470cc7b8949b97e
Jeff Patton <jeffrey@patton-tech.com>
2023-12-29 08:26:21 -0600

    Simple serialization interface for #13

---
+++ Pipelines/Serializers/ISerializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModPosh.Pipelines.Serializers
+{
+    public interface ISerializer
+    {
+        string Serialize<T>(T obj);
+    }
+}
