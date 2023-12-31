fb3c8e5795cad381dce0e5f6a8fb6ba84f0c1340
Jeff Patton <jeffrey@patton-tech.com>
2023-12-31 00:29:23 -0600

    Adding the basic job class

---
+++ Pipelines/Gha/Job.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModPosh.Pipelines.Gha
+{
+    public class Job
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string If { get; set; } = string.Empty;
+        public string RunsOn { get; set; } = string.Empty;
+        public List<Step> Steps { get; set; } = new List<Step>();
+    }
+}
