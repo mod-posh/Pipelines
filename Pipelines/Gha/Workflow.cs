2bc4d49b864c70007672c619e23f40f67955eab3
Jeff Patton <jeffrey@patton-tech.com>
2023-12-31 00:29:11 -0600

    Adding basic Workflow class

---
+++ Pipelines/Gha/Workflow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModPosh.Pipelines.Gha
+{
+    public class Workflow
+    {
+        public string Name { get; set; } = string.Empty;
+        public string RunName { get; set; } = string.Empty;
+        public List<Gha.Job> Jobs { get; set; } = new List<Job>();
+    }
+}
