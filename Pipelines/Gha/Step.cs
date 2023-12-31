bbddff514137e2c2cc54be6e302353d24bd3aedf
Jeff Patton <jeffrey@patton-tech.com>
2023-12-31 00:29:33 -0600

    Added step class

---
+++ Pipelines/Gha/Step.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModPosh.Pipelines.Gha
+{
+    public class Step
+    {
+        public string Id { get; set; } = string.Empty;
+        public string If { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Uses { get; set; } = string.Empty;
+        public string Run { get; set; } = string.Empty;
+        public Dictionary<string, string> With { get; set; } = new Dictionary<string, string>();
+    }
+}
