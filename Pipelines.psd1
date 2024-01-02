606a46ac09daffb6b3765f63079aa3c8d8fdcaf7
Jeff Patton <jeffrey@patton-tech.com>
2024-01-01 21:42:22 -0600

    Created Cmdlets and updated manifest #38, #39, #40

--- Pipelines.psd1
+++ Pipelines.psd1
@@ -72,7 +72,8 @@
     # FunctionsToExport = @()
 
     # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
-    CmdletsToExport    = @('New-Pipeline', 'New-Stage', 'New-Job', 'New-Template', 'New-Pool')
+    CmdletsToExport    = @('New-AdoPipeline','New-AdoStage','New-AdoJob','New-AdoTemplate','New-AdoPool',
+                           'New-GhaWorkflow','New-GhaJob','New-GhaStep')
 
     # Variables to export from this module
     VariablesToExport  = '*'
---
+++ Pipelines/Cmdlets/NewGhaWorkflow.cs
@@ -0,0 +1,29 @@
+﻿using ModPosh.Pipelines.Gha;
+using System.Collections;
+using System.Management.Automation;
+
+namespace ModPosh.Pipelines.Cmdlets
+{
+    [Cmdlet(VerbsCommon.New, "GhaWorkflow", HelpUri = "https://github.com/mod-posh/Pipelines/blob/v2.0.0.0/docs/New-GhaWorkflow.md#new-ghaworkflow")]
+    [OutputType(typeof(Workflow))]
+    [CmdletBinding(PositionalBinding = true)]
+    public class NewGhaWorkflow : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
+        public string Name { get; set; } = string.Empty;
+        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Default")]
+        public string RunName { get; set; } = string.Empty;
+        [Parameter(Mandatory = false, Position = 2, ParameterSetName = "Default")]
+        public Gha.Job[] Jobs { get; set; } = Array.Empty<Gha.Job>();
+
+        protected override void BeginProcessing()
+        {
+            Workflow workflow = new Workflow(Name);
+            if (RunName != null)
+                workflow.RunName = RunName;
+            if (Jobs.Length > 0)
+                workflow.Jobs = Jobs.ToList();
+            WriteObject(workflow);
+        }
+    }
+}
